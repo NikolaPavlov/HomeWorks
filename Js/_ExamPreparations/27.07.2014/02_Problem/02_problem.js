894a2cf21eecd59fca13959388fc395a2a605468
NikolaPavlov <rastamandito@mail.bg>
2014-11-18 11:29:58 +0200

    from laptop

---
+++ Js/_ExamPreparations/27.07.2014/02_Problem/02_problem.js
@@ -0,0 +1,16 @@
+function revealTriangles (input) {
+	//translate input into arr
+	var output = [];
+	output = input[0].split(' ');
+	console.log(output);
+}
+
+
+
+
+
+
+//test here bitchez
+revealTriangles('abcdexgh
+bbbdxxxh
+abcxxxxx');
