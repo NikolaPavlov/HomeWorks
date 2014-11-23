046c307564420070b6f8ccc291ec4a6ccca7cb64
NikolaPavlov <nikola.pavlov1955@gmail.com>
2014-11-23 20:52:31 +0200

    preparation exam

--- Js/_ExamPreparations/29.07.2014/problem_03/03_biggestTableRow.js
+++ Js/_ExamPreparations/29.07.2014/problem_03/03_biggestTableRow.js
@@ -1,27 +1,56 @@
 function biggestRowSum (arrStr) {
-	var nums = [];
-	for (var i = 0; i < arrStr.length; i++) {
-		nums.push(arrStr[0].split(/\D/g));
+	// var regex = /\d+\.\d+|-/g;
+	var regex = /\-?[\d\.]+/g;
+
+	var maxSum = Number.MIN_VALUE;
+	var numsAsString = '';
+
+	
+	for (var i = 2; i < arrStr.length - 1; i++) {
+		var sum = 0;
+		var nums = arrStr[i].match(regex);
+		var numsRightSide = [];
+		for (var j = 0; j < 3; j++) {		
+			if (nums[j] != '-') {
+				sum += Number(nums[j]);
+				numsRightSide.push(nums[j]);
+			}
+			if (sum > maxSum) {
+				maxSum = sum;
+				numsAsString = maxSum + ' = ' + numsRightSide.join(' + ');
+			}
+		}
 	}
-		console.log(nums);
-		console.log(arrStr.length);
+	if (maxSum == Number.MIN_VALUE) {
+		console.log('no data');
+	} else {
+		console.log(numsAsString);
+	}
 }
 
 
 
 
-// biggestRowSum('<table>',
+
+
+
+
+
+
+
+
+// biggestRowSum(['<table>',
 // '<tr><th>Town</th><th>Store1</th><th>Store2</th><th>Store3</th></tr>',
 // '<tr><td>Sofia</td><td>26.2</td><td>8.20</td><td>-</td></tr>',
 // '<tr><td>Varna</td><td>11.2</td><td>18.00</td><td>36.10</td></tr>',
 // '<tr><td>Plovdiv</td><td>17.2</td><td>12.3</td><td>6.4</td></tr>',
 // '<tr><td>Bourgas</td><td>-</td><td>24.3</td><td>-</td></tr>',
-// '</table>');
+// '</table>']);
 
-// biggestRowSum('<table>
-// <tr><th>Town</th><th>Store1</th><th>Store2</th><th>Store3</th></tr>
-// <tr><td>Sofia</td><td>26.2</td><td>8.20</td><td>-</td></tr>
-// <tr><td>Varna</td><td>11.2</td><td>18.00</td><td>36.10</td></tr>
-// <tr><td>Plovdiv</td><td>17.2</td><td>12.3</td><td>6.4</td></tr>
-// <tr><td>Bourgas</td><td>-</td><td>24.3</td><td>-</td></tr>
-// </table>');
+biggestRowSum(['<table>',
+'<tr><th>Town</th><th>Store1</th><th>Store2</th><th>Store3</th></tr>',
+'<tr><td>Sofia</td><td></td><td>8.20</td><td>-</td></tr>',
+'<tr><td>Varna</td><td>11.2</td><td>18.00</td><td>36.10</td></tr>',
+'<tr><td>Plovdiv</td><td>17.2</td><td>12.3</td><td>6.4</td></tr>',
+'<tr><td>Bourgas</td><td>-</td><td>3000.3</td><td>-</td></tr>',
+'</table>']);
