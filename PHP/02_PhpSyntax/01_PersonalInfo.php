8d4dff5e39176b1dc09e317566748a45bc28b0d3
NikolaPavlov <rastamandito@mail.bg>
2014-12-02 16:15:57 +0200

    add php from laptop

---
+++ PHP/02_PhpSyntax/01_PersonalInfo.php
@@ -0,0 +1,8 @@
+<?php 
+$firstName = Petarcho;
+$lastName = Kukov;
+$age = 33;
+$fullName = $firstName . ' ' . $lastName;
+
+echo "My name is $fullName and I am $age years old.";
+?>
---
+++ PHP/02_PhpSyntax/02_SumTwoNumbers.php
@@ -0,0 +1,21 @@
+<?php 
+	$num1 = 2;
+	$num2 = 5;
+	$answer = $num1 + $num2;
+	echo '$firstNum + $secondNum = '. $num1 . ' + ' . $num2 . ' = ' . round($answer, 2);
+
+	echo '<br>----------------------------------<br>';
+	
+	$num1 = 1.567808;
+	$num2 = 0.356;
+	$answer = $num1 + $num2;
+	echo '$firstNum + $secondNum = '. $num1 . ' + ' . $num2 . ' = ' . round($answer, 2);
+
+	echo '<br>----------------------------------<br>';
+
+	$num1 = 1234.5678;
+	$num2 = 333;
+	$answer = $num1 + $num2;
+	echo '$firstNum + $secondNum = '. $num1 . ' + ' . $num2 . ' = ' . round($answer, 2);
+
+ ?>
