4b69c41e33512b2d8d661c703ccb507c52be204b
NikolaPavlov <rastamandito@mail.bg>
2015-01-29 01:36:11 +0200

    added namespace homework 1st and 2nd problems

---
+++ C#/02_NamerspacesAndStaticFields/01_Point3D/PointTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Point
+{
+    public class PointTest
+    {
+        static void Main()
+        {
+            Point3D p1 = new Point3D(3, 3, 3);
+            Point3D p2 = new Point3D(x: 1, z: 2);
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p2.ToString());
+            Console.WriteLine(Point3D.StartingPoint);
+        }
+    }
+}
---
+++ C#/02_NamerspacesAndStaticFields/02_DistanceCalculator/DistanceCalculatorTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Point;
+
+class DistanceCalculatorTest
+{
+    static void Main()
+    {
+        // Calculate the distance between The Earth and The Moon AWSOME!
+        Point3D Earth = new Point3D(1, 1, 1);
+        Point3D TheMoon = new Point3D(100, 100, 100);
+        Console.WriteLine(DistanceCalculator.CalculateDistance(Earth, TheMoon));
+    }
+}
