1ac44c7f252648686f07b5eea94a2200c483383c
NikolaPavlov <rastamandito@mail.bg>
2015-02-14 16:47:45 +0200

    add functional programming shits

---
+++ C#/06_FunctionalPrograming/03_ClassStudent/Student.cs
@@ -0,0 +1,145 @@
+﻿namespace _03_ClassStudent
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class Student
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private int facultyNumber;
+        private string phone;
+        private string email;
+        private IList<int> marks;
+        private int groupNumber;
+
+        // Constructor
+        public Student(string firstName, string lastName, int age, int facultyNumber, string phone,
+            string email, IList<int> marks, int groupNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.FacultyNumber = facultyNumber;
+            this.Phone = phone;
+            this.Email = email;
+            this.Marks = marks;
+            this.GroupNumber = groupNumber;
+        }
+
+        // Prop
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                Validator.CheckForNullOrEmptyString(value, "FirstName");
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                Validator.CheckForNullOrEmptyString(value, "LastName");
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                Validator.CheckForNegativeOrZero(value, "Age");
+                this.age = value;
+            }
+        }
+
+        public int FacultyNumber
+        {
+            get
+            {
+                return this.facultyNumber;
+            }
+            set
+            {
+                Validator.CheckForNegativeOrZero(value, "FacultyNumber");
+                this.facultyNumber = value;
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return this.phone;
+            }
+            set
+            {
+                Validator.CheckForNullOrEmptyString(value, "Phone");
+                this.phone = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                Validator.CheckForNullOrEmptyString(value, "Email");
+                this.email = value;
+            }
+        }
+
+        public IList<int> Marks
+        {
+            get
+            {
+                return this.marks;
+            }
+            set
+            {
+                Validator.CheckForNull(value, "Marks");
+                this.marks = value;
+            }
+        }
+
+        public int GroupNumber
+        {
+            get
+            {
+                return this.groupNumber;
+            }
+            set
+            {
+                Validator.CheckForNegativeOrZero(value, "Group number");
+                this.groupNumber = value;
+            }
+        }
+
+        // Methods
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(this.FirstName + " " + this.LastName);
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#/06_FunctionalPrograming/03_ClassStudent/TestStudents.cs
@@ -0,0 +1,52 @@
+﻿namespace _03_ClassStudent
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    
+    class TestStudents
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>()
+                {
+                    new Student("Iosif", "Iordanov", 22, 3243543, "+359332244", "email@mail.bg", new List<int>(){3,3,3,3,3}, 1),
+                    new Student("Rossy", "Petrova", 21, 4342234, "+334359332244", "eeemail@mail.bg", new List<int>(){6,6,6,6,6}, 2),
+                    new Student("Hristo", "Stoichkov", 25, 324353243, "+359332242344", "email@mail.bg", new List<int>(){3,2,2,3,2}, 3),
+                    new Student("Aron", "Shwartz", 44, 132435343, "+359332242443", "email@maail.bg", new List<int>(){3,2,3,3,3}, 2),
+                    new Student("Linux", "Rulz", 99, 223243543, "+359332244234", "email@maaail.bg", new List<int>(){3,3,6,6,6}, 2),
+                    new Student("Suck", "Microsoft", 11, 773243543, "+359332242344", "email@maaaaail.bg", new List<int>(){3,2,6,3,6}, 111),
+                    new Student("Nakov", "Bradichka", 22, 123243543, "+359332234344", "email@mailll.bg", new List<int>(){3,3,6,6,6}, 12),
+                    new Student("Dumb", "Lector", 16, 133243543, "+359332242344", "email@maaaaail.bg", new List<int>(){3,3,6,6,6}, 11),
+                }; 
+
+            // Problem 04 Group students by group
+            Console.WriteLine("Problem 04 Group students by group:");
+            var studentsByGroup = students.Where(s => s.GroupNumber == 2);
+            foreach (var student in studentsByGroup)
+            {
+                Console.WriteLine(student.ToString()); 
+            }
+
+            Console.WriteLine();
+
+            // Problem 05 Students by first and last name
+            Console.WriteLine("Problem 05 Students by first and last name");
+            var studentsByNames = students.Where(s => string.Compare(s.FirstName, s.LastName) < 0);
+            foreach (var student in studentsByNames)
+            {
+                Console.WriteLine(student.ToString()); 
+            }
+
+            Console.WriteLine();
+
+            // Problem 06 Students by age
+            Console.WriteLine("Problem 06 Students by age");
+            var studentsByAge = students.Where(s => s.Age >= 18 && s.Age <= 24);
+            foreach (var student in studentsByAge)
+            {
+                Console.WriteLine(student.ToString()); 
+            }
+        }
+    }
+}
